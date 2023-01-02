fa9340e1757dcb873b24c8e8d38c5727fa4e15c7
Kazi <daglarov.k@gmail.com>
2023-01-02 22:26:25 +0300

    добавлена авторизация

--- cinemaSite/cinema/views.py
+++ cinemaSite/cinema/views.py
@@ -1,4 +1,6 @@
-
+from django.contrib.auth import logout, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy
@@ -49,8 +51,6 @@ class AddMovie(LoginRequiredMixin, DataMixin, CreateView):
 def series(request):
     pass
 
-def login(request):
-    pass
 
 
 class MovieShow(DataMixin, DetailView):
@@ -89,3 +89,24 @@ class RegisterUser(DataMixin, CreateView):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title='Регистрация')
         return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'cinema/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
